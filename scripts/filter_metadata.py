52fb4b7531b8d7e003f83fff06756091b2b21d9b
Anderson Brito <andersonfbrito@gmail.com>
2021-03-17 00:43:46 -0300

    Update filter_metadata.py

--- scripts/filter_metadata.py
+++ scripts/filter_metadata.py
@@ -62,8 +62,6 @@ if __name__ == '__main__':
     # nextstrain metadata
     dfN = pd.read_csv(metadata1, encoding='utf-8', sep='\t', dtype='str')
     try:
-        dfN = dfN[['strain', 'pangolin_lineage', 'gisaid_epi_isl', 'genbank_accession', 'date', 'country', 'division', 'location',
-                      'region_exposure', 'country_exposure', 'division_exposure', 'originating_lab', 'submitting_lab']]
         dfN.insert(5, 'iso', '')
     except:
         pass
