0e1d203ca909e6803506706f50e9209bad040387
Anderson Brito <andersonfbrito@gmail.com>
2021-03-28 18:12:59 -0300

    new scripts

--- scripts/filter_metadata.py
+++ scripts/filter_metadata.py
@@ -25,12 +25,11 @@ if __name__ == '__main__':
     output1 = args.output1
     output2 = args.output2
 
-    # path = "/Users/anderson/GLab Dropbox/Anderson Brito/projects/ncov_nfl/nextstrain/batch01_20201012e/pre-analyses/"
-    # genomes = path + 'temp_sequences.fasta'
-    # metadata1 = path + 'metadata_nextstrain.tsv'
-    # metadata2 = path + 'SC2_Project2.xlsx'
-    # output1 = path + 'metadata_filtered.tsv'
-    # output2 = path + 'sequences.fasta'
+    # genomes = path + 'pre-analyses/temp_sequences.fasta'
+    # metadata1 = path + 'pre-analyses/metadata_nextstrain.tsv'
+    # metadata2 = path + 'pre-analyses/SC2_Project2.xlsx'
+    # output1 = path + 'pre-analyses/metadata_filtered.tsv'
+    # output2 = path + 'pre-analyses/sequences.fasta'
 
     pd.set_option('max_columns', 100)
 
@@ -61,163 +60,155 @@ if __name__ == '__main__':
 
     # nextstrain metadata
     dfN = pd.read_csv(metadata1, encoding='utf-8', sep='\t', dtype='str')
-    try:
-        dfN.insert(5, 'iso', '')
-    except:
-        pass
+    dfN.insert(5, 'iso', '')
     dfN['category'] = ''
     dfN['batch'] = ''
     dfN['sequencing_date'] = ''
 
     dfN.fillna('', inplace=True)
-    lColumns = dfN.columns.values  # list of column in the original metadata file
+    # lColumns = dfN.columns.values  # list of column in the original metadata file
+    list_columns = dfN.columns.values  # list of column in the original metadata file
 
     # Lab genomes metadata
     dfL = pd.read_excel(metadata2, index_col=None, header=0, sheet_name=0,
                         # 'sheet_name' must be changed to match the Excel sheet name
                         converters={'sample': str, 'collection-date': str, 'category': str, 'batch': str})  # this need to be tailored to your lab's naming system
     dfL.fillna('', inplace=True)
-    dfL.set_index('id', inplace=True)
 
-    notFound = []
-    lstNewMetadata = []
+
+    # output dataframe
+    outputDF = pd.DataFrame(columns=list_columns)
     found = []
     lab_label = {}
-    for id in sequences.keys():
-        # check nextstrain metadata first
-        dRow = {}
-        if id in dfN['strain'].to_list() and len(id) > 4:
-            fields = {column: '' for column in lColumns}
-            row = dfN.loc[lambda dfN: dfN['strain'] == id]
 
-            strain = row.strain.values[0]
-            country = row.country.values[0]
-            division = row.division.values[0]
-            location = row.location.values[0]
+    # process metadata from excel sheet
+    for idx, row in dfL.iterrows():
+        id = dfL.loc[idx, 'id']
+        if id in sequences:
+            dict_row = {}
+            for col in list_columns:
+                dict_row[col] = ''
+                if col in row:
+                    dict_row[col] = dfL.loc[idx, col]  # add values to dictionary
 
-            country_exposure = row.country_exposure.values[0].strip()
-            # print('\t > ', country_exposure, country)
+            if dict_row['location'] in ['', None]:
+                dict_row['location'] = dfL.loc[idx, 'location']
+
+            collection_date = ''
+            if len(str(dict_row['date'])) > 1:
+                collection_date = dict_row['date'].split(' ')[0].replace('.', '-').replace('/', '-')
+                dict_row['date'] = collection_date
+
+            dfL.loc[idx, 'country'] = 'USA'
+            dfL.loc[idx, 'region'] = 'North America'
+            dfL.loc[idx, 'country_exposure'] = ''
+            dfL.loc[idx, 'division_exposure'] = ''
 
             # fix exposure
-            if country_exposure in ['', None]:  # ignore travel cases
-                # print('\t - ', country_exposure, country)
-                country_exposure = country
+            columns_exposure = ['country_exposure', 'division_exposure']
+            for level_exposure in columns_exposure:
+                level = level_exposure.split('_')[0]
+                dict_row[level_exposure] = dfL.loc[idx, level_exposure]
+                if dict_row[level_exposure] in ['', None]:
+                    if level_exposure == 'country_exposure':
+                        dict_row[level_exposure] = dict_row[level]
+                    else:
+                        if dict_row['country_exposure'] != dfL.loc[idx, 'country']:
+                            dict_row[level_exposure] = dict_row['country_exposure']
+                        else:
+                            dict_row[level_exposure] = dict_row[level]
 
-            division_exposure = row.division_exposure.values[0].strip()
-            # print('\t - ', division, division_exposure)
+            if row['state'] == '':
+                code = 'un'  # change this line to match the acronym of the most likely state of origin if the 'State' field is unknown
+            else:
+                code = row['state']
+            strain = code + '/' + row['sample'] + '/' + id # new strain name
 
-            if division_exposure in ['', None]:  # ignore travel cases
-                # print('\t - ', division, division_exposure)
-                division_exposure = division
+            dict_row['strain'] = strain
+            dict_row['submitting_lab'] = 'Grubaugh Lab - Yale School of Public Health'
+            dict_row['authors'] = 'Fauver et al'
+            dict_row['category'] = row['category']
+            dict_row['batch'] = 'Batch' + str('0' * (3 - len(row['batch']))) + row['batch']
+            dict_row['sequencing_date'] = row['sequencing-collection-date'].strftime('%Y-%m-%d')
 
-            if len(country) < 2:
-                row.country.values[0] = ''
-            if len(division) < 2:
-                row.division.values[0] = country
+            # add lineage
+            lineage = ''
+            if dfL.loc[idx, 'pango_lineage'] != '':
+                lineage = dfL.loc[idx, 'pango_lineage']
+            dict_row['pango_lineage'] = lineage
 
-            iso = get_iso(country_exposure)
-            row.iso.values[0] = iso  # needed for exporting a renaming file
-            date = row.date.values[0]
-
-            lValues = row.values[0]
-            for field, value in zip(fields.keys(), lValues):
-                # print(id)
-                if value in ['', np.nan, None]:
-                    value = ''
-
-                fields[field] = value
-            if country == '':
-                continue
-
-            dRow[id] = fields
             found.append(strain)
-            # print('Exporting metadata for ' + id)
+            lab_label[id] = strain
 
-        # check lab's metadata otherwise
-        if id not in dRow.keys():
-            # check lab metadata
-            if len(id) < 5:
-                id = str(id)
-                lab_label[id] = ''
+            outputDF = outputDF.append(dict_row, ignore_index=True)
 
-                if id in dfL.index:
-                    fields = {column: '' for column in lColumns}
-                    row = dfL.loc[id]
-                    if row['state'] == '':
-                        code = 'XX'  # change this line to match the acronym of the most likely state of origin if the 'State' field is unknown
-                    else:
-                        code = row['state']
-                    strain = code + '/' + row['sample'] + '/' + id # new strain name
+    # process metadata from TSV
+    dfN = dfN[dfN['strain'].isin(sequences.keys())]
+    for idx, row in dfN.iterrows():
+        strain = dfN.loc[idx, 'strain']
+        if strain in sequences:
+            if strain in outputDF['strain'].to_list():
+                continue
+            dict_row = {}
+            for col in list_columns:
+                dict_row[col] = ''
+                if col in row:
+                    dict_row[col] = dfN.loc[idx, col]
 
-                    if strain not in found:
-                        gisaid_epi_isl = ''
-                        pangolin_lineage = ''
-                        genbank_accession = ''
-                        if len(str(row['collection-date'])) > 1:
-                            date = row['collection-date'].split(' ')[0].replace('.', '-').replace('/', '-')
-                        else:
-                            date = ''
+            # fix exposure
+            columns_exposure = ['country_exposure', 'division_exposure']
+            for level_exposure in columns_exposure:
+                level = level_exposure.split('_')[0]
+                if dict_row[level_exposure] in ['', None]:
+                    dict_row[level_exposure] = dict_row[level]
 
-                        country = row['country']
-                        division = row['division']
-                        if row['location'] in ['', '?', 'N/A']:
-                            location = ''
-                        else:
-                            location = str(row['location'])
+            dict_row['iso'] = get_iso(dict_row['country'])
+            found.append(strain)
 
-                        region_exposure = 'North America'
-                        country_exposure = 'USA'
-                        iso = get_iso(country_exposure)
-                        division_exposure = division
-                        originating_lab = row['lab']
-                        submitting_lab = 'Grubaugh Lab - Yale School of Public Health'  # change this line to match you lab's name
-                        category = row['category']
-                        batch = 'Batch' + str('0' * (3 - len(row['batch']))) + row['batch']
-                        sequencing_date = row['sequencing-collection-date'].strftime('%Y-%m-%d')
-
-                        lValues = [strain, pangolin_lineage, gisaid_epi_isl, genbank_accession, date, iso, country, division,
-                                   location, region_exposure, country_exposure, division_exposure, originating_lab, submitting_lab,
-                                   category, batch, sequencing_date]
-                        for field, value in zip(fields.keys(), lValues):
-                            fields[field] = value
-                        dRow[id] = fields
-                        found.append(strain)
-
-                        if id in lab_label.keys():
-                            lab_label[id] = strain
-                    else:
-                        continue
-
-            else:  # Assign 'NA' if no metadata is available
-                notFound.append(id)
-        lstNewMetadata = lstNewMetadata + list(dRow.values())
-
+            outputDF = outputDF.append(dict_row, ignore_index=True)
 
 
     # write new metadata files
-    outputDF = pd.DataFrame(lstNewMetadata, columns=list(lColumns))
+    outputDF = outputDF.drop(columns=['region'])
     outputDF.to_csv(output1, sep='\t', index=False)
 
-    if len(notFound) > 0:
-        print('\nPlease check for inconsistencies (see above).')
 
-    # write fasta file
+    # write sequence file
     exported = []
-    print('\n### Exporting genomes and metadata')
-    print('\t Exporting all selected lab sequences, publicly available genomes and metadata')
     with open(output2, 'w') as outfile2:
-        # export new fasta entries
+        # export new metadata lines
         for id, sequence in sequences.items():
-            if len(id) < 5:
+            if id in lab_label:
                 if lab_label[id] not in exported:
                     entry = '>' + lab_label[id] + '\n' + sequence + '\n'
                     outfile2.write(entry)
-                    print('\t\t* Newly sequenced genome and metadata: ' + id)
+                    print('* Exporting newly sequenced genome and metadata for ' + id)
                     exported.append(lab_label[id])
-            elif len(id) > 4:
+            else:
                 if id not in exported:
                     entry = '>' + id + '\n' + sequence + '\n'
                     outfile2.write(entry)
                     exported.append(id)
+
+
+
+    # # write fasta file
+    # exported = []
+    # print('\n### Exporting genomes and metadata')
+    # print('\t Exporting all selected lab sequences, publicly available genomes and metadata')
+    # with open(output2, 'w') as outfile2:
+    #     # export new fasta entries
+    #     for id, sequence in sequences.items():
+    #         if len(id) < 5:
+    #             if lab_label[id] not in exported:
+    #                 entry = '>' + lab_label[id] + '\n' + sequence + '\n'
+    #                 outfile2.write(entry)
+    #                 print('\t\t* Newly sequenced genome and metadata: ' + id)
+    #                 exported.append(lab_label[id])
+    #         elif len(id) > 4:
+    #             if id not in exported:
+    #                 entry = '>' + id + '\n' + sequence + '\n'
+    #                 outfile2.write(entry)
+    #                 exported.append(id)
 
 print('\nMetadata file successfully reformatted and exported!\n')
